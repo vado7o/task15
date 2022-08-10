891b0590b0cb76cbe2d6fd7da1f48388352b706a
Vadim Pudov <pvb@rambler.ru>
2022-08-10 20:37:44 +0700

    task15

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.");
+
+var num = 0;
+
+while(true) {
+Console.WriteLine("Введите любое целое число от 1 до 7 включительно: ");
+string? str = Console.ReadLine();
+if(int.TryParse(str, out int number)) {
+    num = number;
+    if(num > 7 || num < 1) {
+        Console.WriteLine("Нет такого дня недели с числом " + num + "!");
+    }
+    else if(num == 6 || num == 7) {
+        Console.WriteLine("Да, день недели под цифрой " + num + " является выходным!");
+        break;
+    }
+    else {
+        Console.WriteLine("Нет, день недели под цифрой " + num + " НЕ является выходным!");
+    break;
+    }
+}
+else Console.WriteLine("Некорректно введено число!");
+}
